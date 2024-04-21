c99b44df7fcdc13c1e73265c1d194113e2ccad02
Mohammed-Nur Nur <mohammed-nur.nur0002@stud.hkr.se>
2024-04-21 19:17:49 +0200

    Added gettes and setter for the player class aswell as a function to reset the score to 0

--- player.py
+++ player.py
@@ -7,3 +7,23 @@ class Player:
         self.name = name
         self.score = score
         self.difficulty_level = difficulty_level
+
+    def get_name(self):
+        """Function returns the name of the player"""
+        return f"{self.name}"
+
+    def set_name(self, name):
+        """Function changes the name of the player"""
+        self.name = name
+
+    def get_score(self):
+        """Function returns the score of the player"""
+        return self.score
+
+    def set_score(self, score):
+        """Function changes the score of the player"""
+        self.score = score
+
+    def reset_score(self):
+        """Function resets the score of the player """
+        self.score = 0
