f2df81f5f5cb77086c427af55576d20e0e845a8a
Mohammed-Nur Nur <mohammed-nur.nur0002@stud.hkr.se>
2024-04-21 18:11:34 +0200

    Added a player class with a constructor

---
+++ player.py
@@ -0,0 +1,9 @@
+"""No module"""
+
+
+class Player:
+    """Class representing a player"""
+    def __init__(self, name, score, difficulty_level):
+        self.name = name
+        self.score = score
+        self.difficulty_level = difficulty_level
