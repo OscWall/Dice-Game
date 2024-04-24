ef9b5e18014dc7b65e366806f6494b8dd21cf42b
UmarFarouk001 <abdulhakeem.umar_farouk_omobolaji0001@stud.hkr.se>
2024-04-24 16:15:00 +0200

    deleted code

--- dice.py
+++ dice.py
@@ -19,20 +19,3 @@ class Dice:
 
     def get_sides(self):
         return self.sides
-
-# Create a six-sided dice
-my_dice = Dice()
-
-# Roll the dice
-result = my_dice.roll()
-print("Result of the roll:", result)
-
-# Get the current value
-current_value = my_dice.get_current_value()
-print("Current value of the dice:", current_value)
-
-# Change the number of sides
-my_dice.set_sides(12)
-
-# Get the new number of sides
-print("Number of sides after modification:", my_dice.get_sides())
