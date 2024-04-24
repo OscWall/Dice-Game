837dd02dbd455c27fc9db2cd23c860e2efbb4d5e
Mohammed-Nur Nur <mohammed-nur.nur0002@stud.hkr.se>
2024-04-24 18:46:41 +0200

    Added comments and changed if-statement

--- dice.py
+++ dice.py
@@ -1,21 +1,27 @@
 import random
 
+
 class Dice:
+    """Class used to create a dice"""
     def __init__(self, sides=6):
         self.sides = sides
         self.current_value = None
 
     def roll(self):
+        """Class used to roll a dice"""
         self.current_value = random.randint(1, self.sides)
         return self.current_value
 
     def get_current_value(self):
+        """Function to get the current value"""
         return self.current_value
 
     def set_sides(self, num_sides):
-        if num_sides < 2:
-            raise ValueError("Number of sides must be at least 2")
+        """Function to change the amount of sides"""
+        if num_sides < 6:
+            raise ValueError("Number of sides must be at least 6")
         self.sides = num_sides
 
     def get_sides(self):
+        """Function to get the number of sides"""
         return self.sides
