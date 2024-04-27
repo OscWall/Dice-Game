a3130a0eb0b6d064c84212dbaa00a3724d9c7837
Mohammed-Nur Nur <mohammed-nur.nur0002@stud.hkr.se>
2024-04-27 15:19:40 +0200

    Added code to the diceHand class

--- diceHand.py
+++ diceHand.py
@@ -16,6 +16,7 @@ class DiceHand():
         score_to_check_dice1 = 0
         score_to_check_dice2 = 0
 
+        # If cpu difficulty is easy
         if player.get_difficulty_level == 1:
             # Make sure the Dice class has a method called "get_sides"
             score_to_check_dice1 = random.randint(1, dice1.get_sides())
@@ -24,8 +25,62 @@ class DiceHand():
             if (score_to_check_dice1 == 1 or score_to_check_dice2 == 1):
                 score += score_to_check_dice1
                 score += score_to_check_dice2
+                player.add_score(score)
             elif (score_to_check_dice1 == 1 and score_to_check_dice2 == 1):
                 player.reset_score()
             else:
                 score += score_to_check_dice1
                 score += score_to_check_dice2
+
+        # If cpu difficulty is medium
+        elif player.get_difficulty_level() == 2:
+            # Make sure the Dice class has a method called "get_sides"
+            score_to_check_dice1 = random.randint(3, dice1.get_sides())
+            # Make sure the Dice class has a method called "get_sides"
+            score_to_check_dice2 = random.randint(3, dice2.get_sides())
+            if (score_to_check_dice1 == 1 or score_to_check_dice2 == 1):
+                score += score_to_check_dice1
+                score += score_to_check_dice2
+            elif (score_to_check_dice1 == 1 and score_to_check_dice2 == 1):
+                player.reset_score()
+            else:
+                score += score_to_check_dice1
+                score += score_to_check_dice2
+
+        # If cpu difficulty is hard
+        else:
+            # Make sure the Dice class has a method called "get_sides"
+            score_to_check_dice1 = random.randint(4, dice1.get_sides())
+            # Make sure the Dice class has a method called "get_sides"
+            score_to_check_dice2 = random.randint(4, dice2.get_sides())
+            if (score_to_check_dice1 == 1 or score_to_check_dice2 == 1):
+                score += score_to_check_dice1
+                score += score_to_check_dice2
+            elif (score_to_check_dice1 == 1 and score_to_check_dice2 == 1):
+                score = player.reset_score()
+            else:
+                score += score_to_check_dice1
+                score += score_to_check_dice2
+        return score
+
+    def roll_pvp(self, player, dice1, dice2):
+        """Function performs rolls for both players"""
+        score = 0
+        score_to_check_dice1 = random.randint(1, dice1.get_sides())
+        score_to_check_dice2 = random.randint(1, dice2.get_sides())
+
+        if (score_to_check_dice1 == 1 or score_to_check_dice2 == 1):
+            score += score_to_check_dice1
+            score += score_to_check_dice2
+            player.add_score(score)
+
+        elif (score_to_check_dice1 == 1 and score_to_check_dice2 == 1):
+            player.reset_score()
+            # condition = False
+
+        else:
+            score += score_to_check_dice1
+            score += score_to_check_dice2
+            player.addScore(score)
+
+        return score
